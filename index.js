d78ada1e05ae603b188717901d2585a5683b9937
Keittah Sewe <kseweo@gmail.com>
2022-12-05 11:48:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2)
+console.log(multiply);
 
-// Write your code here
+let random = Math.ceil(Math.random());
+
+let num3 = 24;
+let num4 = 5;
+let mod = Math.trunc(num3 % num4);
+console.log(mod);
+
+let max = Math.max(11,12,20)
